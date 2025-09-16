1d7f564224e2754ae47f5297f624b07593d96b47
szetol0718 <szetol@bu.edu>
2025-09-16 15:53:05 -0400

    setup lab2

---
+++ src/labs/stealth/agents/BFSMazeAgent.java
@@ -0,0 +1,38 @@
+package src.labs.stealth.agents;
+
+// SYSTEM IMPORTS
+import edu.bu.labs.stealth.agents.MazeAgent;
+import edu.bu.labs.stealth.graph.Vertex;
+import edu.bu.labs.stealth.graph.Path;
+
+
+import edu.cwru.sepia.environment.model.state.State.StateView;
+
+
+import java.util.HashSet;       // will need for bfs
+import java.util.Queue;         // will need for bfs
+import java.util.LinkedList;    // will need for bfs
+import java.util.Set;           // will need for bfs
+
+
+// JAVA PROJECT IMPORTS
+
+
+public class BFSMazeAgent
+    extends MazeAgent
+{
+
+    public BFSMazeAgent(int playerNum)
+    {
+        super(playerNum);
+    }
+
+    @Override
+    public Path search(Vertex src,
+                       Vertex goal,
+                       StateView state)
+    {
+        return null;
+    }
+
+}
---
+++ src/labs/stealth/agents/DFSMazeAgent.java
@@ -0,0 +1,37 @@
+package src.labs.stealth.agents;
+
+// SYSTEM IMPORTS
+import edu.bu.labs.stealth.agents.MazeAgent;
+import edu.bu.labs.stealth.graph.Vertex;
+import edu.bu.labs.stealth.graph.Path;
+
+
+import edu.cwru.sepia.environment.model.state.State.StateView;
+
+
+import java.util.HashSet;   // will need for dfs
+import java.util.Stack;     // will need for dfs
+import java.util.Set;       // will need for dfs
+
+
+// JAVA PROJECT IMPORTS
+
+
+public class DFSMazeAgent
+    extends MazeAgent
+{
+
+    public DFSMazeAgent(int playerNum)
+    {
+        super(playerNum);
+    }
+
+    @Override
+    public Path search(Vertex src,
+                       Vertex goal,
+                       StateView state)
+    {
+        return null;
+    }
+
+}
