27ec9616574d04ef99382c0d327295834e540799
szetol0718 <szetol0718@icloud.com>
2025-11-14 11:32:11 +0800

    done othello save

--- src/pas/othello/ordering/MoveOrderer.java
+++ src/pas/othello/ordering/MoveOrderer.java
@@ -2,37 +2,25 @@ package src.pas.othello.ordering;
 
 // SYSTEM IMPORTS
 import edu.bu.pas.othello.traversal.Node;
-import src.pas.othello.heuristics.Heuristics;
+import java.util.Collections;
+import java.util.List;
 
 // JAVA PROJECT IMPORTS
-import java.util.List;
-import java.util.Collections;
-import java.util.Comparator; // Import Comparator if not using lambda shorthand
-
-public class MoveOrderer
-    extends Object
-{
-    public static List<Node> orderChildren(List<Node> children)
-    {
-        // Sort children in DESCENDING order of their heuristic value (best moves first)
-        Collections.sort(children, new Comparator<Node>() {
-            @Override
-            public int compare(Node n1, Node n2) {
-                double h1 = Heuristics.calculateHeuristicValue(n1);
-                double h2 = Heuristics.calculateHeuristicValue(n2);
-                
-                // h2 vs h1 ensures descending order (higher score first)
-                int primaryCompare = Double.compare(h2, h1);
-
-                if (primaryCompare != 0) {
-                    return primaryCompare;
-                }
 
 
-                return Integer.compare(n1.hashCode(), n2.hashCode());
-            }
-        });
-        
+public class MoveOrderer {
+
+    /**
+     * Orders the list of child nodes before search expansion.
+     * Currently, this implementation simply randomizes the list
+     * to avoid deterministic expansion order.
+     *
+     * @param children the list of child nodes to reorder
+     * @return the reordered (shuffled) list of nodes
+     */
+    public static List<Node> orderChildren(List<Node> children) {
+
+        Collections.shuffle(children);
         return children;
     }
 }
